c189f9623520e62c1d95accb36d33e388b08ccae
Мария Корогодова <maria@desktop-41ovhou.ad.guu.ru>
2021-11-16 23:55:15 +0300

    initial commit

---
+++ swifty-companion/swifty-companion/View/ProfileViewController.swift
@@ -0,0 +1,8 @@
+//
+//  ProfileViewController.swift
+//  swifty-companion
+//
+//  Created by Мария Корогодова on 16.11.2021.
+//
+
+import Foundation
---
+++ swifty-companion/swifty-companion/View/RootViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  swifty-companion
+//
+//  Created by Мария Корогодова on 16.11.2021.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+    }
+
+
+}
+
